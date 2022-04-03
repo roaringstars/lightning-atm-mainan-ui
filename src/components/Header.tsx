6d5e84ff5f00f6ee9fc48689d0081cd651a7884c
Nathanael Fredericko <nathanaelfredericko@gmail.com>
2022-04-03 15:58:43 +0700

    Minor update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const Header = () => {
     return (
         <>
-            <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+            <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js"></script>
             <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
             <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
 
