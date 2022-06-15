572ee37744e457cd98d07ca3026c71c2743bc9f4
roaringstars <campussurvival@protonmail.com>
2022-06-15 12:57:57 +0700

    Fix data plot

--- src/pages/grafik-biaya-penukaran.tsx
+++ src/pages/grafik-biaya-penukaran.tsx
@@ -68,20 +68,20 @@ const GrafikBiayaPenukaran = () => {
             })
             .then(data => {
                 // setData(data.data);
-                const labels = data.data.label;
+                const labels = data.data.label.reverse();
                 setChartData({
                     labels,
                     datasets: [
                         {
                             label: 'Harga Bitcoin',
-                            data: data.data.rate,
+                            data: data.data.rate.reverse(),
                             borderColor: 'rgba(100, 100, 100, 0.1)',
                             backgroundColor: 'rgba(100, 100, 100, 0.1)',
                             yAxisID: 'y',
                         },
                         {
                             label: 'Potongan Biaya (%)',
-                            data: data.data.fee,
+                            data: data.data.fee.reverse(),
                             borderColor: 'rgb(53, 162, 235)',
                             backgroundColor: 'rgba(53, 162, 235, 0.5)',
                             yAxisID: 'y1',
